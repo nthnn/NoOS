0078dab5d985df71798c53ffebe87bf9dd4686ec
Nathanne Isip <nathanneisip@gmail.com>
2024-05-06 16:51:42 +0800

    Device and port address definitions header file.

---
+++ include/noos_addr.h
@@ -0,0 +1,9 @@
+#ifndef NOOS_ADDR_H
+#define NOOS_ADDR_H
+
+#define NOOS_VIDEO_ADDRESS          0xB8000
+
+#define NOOS_KBD_STATUS_REGISTER    0x64
+#define NOOS_KBD_DATA_REGISTER      0x60
+
+#endif
